8c33a2f4078b0f27572f8c321097f44dbbd41915
Nemanja Tozic <nemanja.tozic@kindredgroup.com>
2020-05-06 11:28:56 +0200

    Removing warning for database

--- app/src/main/java/com/nemanjatozic/gw2buddy/db/AppDatabase.kt
+++ app/src/main/java/com/nemanjatozic/gw2buddy/db/AppDatabase.kt
@@ -7,9 +7,12 @@ import androidx.room.RoomDatabase
 import com.nemanjatozic.gw2buddy.db.dao.ApiTokenDao
 import com.nemanjatozic.gw2buddy.db.entity.ApiTokenEntity
 
+private const val DB_FILE_NAME = "gw2_buddy.db"
+
 @Database(
     entities = [ApiTokenEntity::class],
-    version = 1
+    version = 1,
+    exportSchema = false
 )
 abstract class AppDatabase protected constructor(): RoomDatabase() {
     abstract fun apiTokenDao(): ApiTokenDao
@@ -20,7 +23,7 @@ abstract class AppDatabase protected constructor(): RoomDatabase() {
         fun getInstance(context: Context) : AppDatabase {
             if (!this::INSTANCE.isInitialized) {
                 INSTANCE = Room
-                    .databaseBuilder(context.applicationContext, AppDatabase::class.java, "gw2_buddy.db")
+                    .databaseBuilder(context.applicationContext, AppDatabase::class.java, DB_FILE_NAME)
                     .build()
             }
             return INSTANCE
